0a4d73962248721ea6326fed64bd53a53076b396
Elshaddi Udonsek <eldlysmart@gmail.com>
2025-10-21 13:35:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5060083943-318a0",
+  "appId": "1:318945906135:web:cb70bc172bd89df5eecf92",
+  "apiKey": "AIzaSyAEzaCLF8N3Uz5ZIsvyHhpohFVgtaXLzB4",
+  "authDomain": "studio-5060083943-318a0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "318945906135"
+};
